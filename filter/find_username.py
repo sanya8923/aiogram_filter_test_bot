4c7a557c7f120166b09f4ebbf5050f0899658b40
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-08 23:56:15 +0400

    add message_with_username

--- filter/find_username.py
+++ filter/find_username.py
@@ -1,18 +1,18 @@
-from typing import Union, Any, Dict
+from typing import Union, Dict, Any
 
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
 
-class HasUsernameFilter(BaseFilter):
+class HasUsernamesFilter(BaseFilter):
     async def __call__(self, message: Message) -> Union[bool, Dict[str, Any]]:
         entities = message.entities or []
 
         found_usernames = [
             item.extract_from(message.text) for item in entities
-            if item.type == 'mention'
+            if item.type == "mention"
         ]
 
         if len(found_usernames) > 0:
-            return {'username': found_usernames}
+            return {"usernames": found_usernames}
         return False
--- handlers/usernames.py
+++ handlers/usernames.py
@@ -3,7 +3,13 @@ from typing import List
 from aiogram import Router, F
 from aiogram.types import Message
 
-from filter.find_username import HasUsernameFilter
+from filter.find_username import HasUsernamesFilter
 
 
 router = Router()
+
+
+@router.message(F.text, HasUsernamesFilter())
+async def message_with_username(message: Message, usernames: List[str]):
+    await message.reply(f"Thanks for link {', '.join(usernames)}")
+
